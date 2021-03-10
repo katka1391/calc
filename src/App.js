ea030193b95d53efc6d66f60dc6f3a6d5e7fbd38
Slavo Kozar <slavo.kozar@gmail.com>
2021-03-10 21:12:29 +0100

    template

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="calc">
+            <div className="display">
+                <input type="number" value={0} disabled/>
+            </div>
+            <div className="buttons">
+                <div className="left">
+                    <button>0</button>
+                    <button className="clear">C</button>
+                    <button className="equal">=</button>
+                    {
+                        ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
+                            const n = i + 1;
+
+                            return (
+                                <button>{n}</button>
+                            )
+                        } )
+                    }
+                </div>
+                <div className="actions">
+                    <button>&divide;</button>
+                    <button>&times;</button>
+                    <button>-</button>
+                    <button>+</button>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default App;
