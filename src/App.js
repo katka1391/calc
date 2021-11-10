0e03ced930fa94ffb1f5874aeb68c445b02eee7f
Slavo Kozar <slavo.kozar@gmail.com>
2021-11-11 00:53:05 +0100

    to do

--- src/App.js
+++ src/App.js
@@ -12,15 +12,24 @@ function App() {
                     <button>0</button>
                     <button className="clear">C</button>
                     <button className="equal">=</button>
-                    {
-                        ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {
-                            const n = i + 1;
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    {/*{*/}
+                    {/*    ( new Array( 9 ).fill( null ) ).map( ( a, i ) => {*/}
+                    {/*        const n = i + 1;*/}
 
-                            return (
-                                <button>{n}</button>
-                            )
-                        } )
-                    }
+                    {/*        return (*/}
+                    {/*            <button>{n}</button>*/}
+                    {/*        )*/}
+                    {/*    } )*/}
+                    {/*}*/}
                 </div>
                 <div className="actions">
                     <button>&divide;</button>
